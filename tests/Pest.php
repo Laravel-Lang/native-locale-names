9bf5a520a5266d776b3d938c0aafadae5f252a21
Andrey Helldar <helldar@dragon-code.pro>
2023-11-10 13:33:22 +0300

    Update copyrights

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * This file is part of the "laravel-lang/locales" project.
+ * This file is part of the "laravel-lang/native-locale-names" project.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
