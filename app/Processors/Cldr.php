360958a4a0b6d964b617b64dea0a5c1b0418d0ee
Andrey Helldar <helldar@dragon-code.pro>
2023-12-29 21:42:08 +0300

    Fixed handling of new locales

--- app/Processors/Cldr.php
+++ app/Processors/Cldr.php
@@ -17,86 +17,14 @@ declare(strict_types=1);
 
 namespace LaravelLang\Dev\Processors;
 
+use Illuminate\Support\Collection;
 use Illuminate\Support\Str;
 use LaravelLang\Dev\Services\Process;
 
 class Cldr extends Processor
 {
     protected array $notSupported = [
-        'ak',
-        'am',
-        'as',
-        'ay',
-        'bho',
-        'bm',
-        'ceb',
-        'ckb',
-        'co',
-        'de_CH',
-        'doi',
-        'dv',
-        'ee',
-        'eo',
-        'fy',
-        'ga',
-        'gd',
-        'gn',
-        'gom',
-        'ha',
-        'haw',
-        'hmn',
-        'ht',
-        'ig',
-        'ilo',
-        'jw',
-        'kri',
-        'ky',
-        'la',
-        'lb',
-        'lg',
-        'ln',
-        'lo',
-        'lus',
-        'mai',
-        'mg',
-        'mi',
-        'ml',
-        'mni_Mtei',
-        'mt',
-        'my',
-        'nso',
-        'ny',
-        'om',
-        'or',
-        'pa',
-        'pt_BR',
-        'qu',
-        'rw',
-        'sa',
-        'sd',
-        'sm',
-        'sn',
-        'so',
-        'sr_Cyrl',
-        'sr_Latn',
-        'sr_Latn_ME',
-        'st',
-        'su',
-        'ta',
-        'te',
-        'ti',
         'tl',
-        'ts',
-        'tt',
-        'uz_Cyrl',
-        'uz_Latn',
-        'xh',
-        'yi',
-        'yo',
-        'zh_CN',
-        'zh_HK',
-        'zh_TW',
-        'zu',
     ];
 
     public function handle(): void
@@ -113,7 +41,10 @@ class Cldr extends Processor
             ])
             ->flatten()
             ->unique()
-            ->filter(fn (string $locale) => ! in_array($locale, $this->notSupported, true))
+            ->when($this->notSupported, fn (Collection $items, array $except) => $items->filter(
+                fn (string $locale) => ! in_array($locale, $except, true)
+            ))
+            ->filter(fn (string $locale) => ! Str::contains($locale, ['-', '_']))
             ->sort()
             ->map(fn (string $locale) => '+' . $locale)
             ->implode(',');
